d0af6c7476e3c06eec72e9d0033581e1a8cd7369
M Abdullah Abbasi <officialabdullah261@gmail.com>
2024-08-04 04:02:30 -0700

    Task3 File Uploaded

---
+++ task3.js
@@ -0,0 +1,29 @@
+
+// Variables Decleration
+var num1;
+var num2;
+
+// Variables Initialization
+num1=20;
+num2=10;
+
+// Arithematic Variables Declaration
+var sum;
+var difference;
+var product;
+var quotient;
+var modulus;
+
+// Arithematic Operations and Arithematic Variables Initialization
+sum= num1+num2;
+difference= num1-num2;
+product= num1*num2;
+quotient= num1/num2;
+modulus= num1%num2;
+
+// Logging the Values of Simpe and Arithematic Variables
+console.log("The addition of " + num1 + " and " + num2 + " is: " + sum);
+console.log("The difference of " + num1 + " and " + num2 + " is: " + difference);
+console.log("The product of " + num1 + " and " + num2 + " is: " + product);
+console.log("The quotient of " + num1 + " and " + num2 + " is: " + quotient);
+console.log("The remainder of " + num1 + " and " + num2 + " is: " + modulus);
